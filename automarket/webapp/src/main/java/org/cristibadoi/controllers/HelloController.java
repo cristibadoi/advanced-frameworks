1b2b76c29479a5f4d9d1aef0120cdc0ac7761b8a
Cristi Badoi <cristibadoi1@gmail.com>
2016-12-21 22:16:10 +0200

    Set up project structure

---
+++ automarket/webapp/src/main/java/org/cristibadoi/controllers/HelloController.java
@@ -0,0 +1,19 @@
+package org.cristibadoi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView helloWorld(ModelMap model) {
+		String message = "Hello World!";
+		return new ModelAndView("welcome", "message", message);
+	}
+	
+}
