418ae4d7faced6bf5075ddbdc86452211d1deb69
Cristi Badoi <cristibadoi1@gmail.com>
2017-01-26 12:25:13 +0200

    remove unused line of code

--- automarket/persistence/repositories/src/main/java/com/cristibadoi/automarket/persistence/repositories/TypeRepository.java
+++ automarket/persistence/repositories/src/main/java/com/cristibadoi/automarket/persistence/repositories/TypeRepository.java
@@ -3,7 +3,6 @@ package com.cristibadoi.automarket.persistence.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.cristibadoi.automarket.persistence.models.TypeModel;
-import com.cristibadoi.automarket.persistence.models.UserModel;
 
 public interface TypeRepository extends CrudRepository<TypeModel, Long> {
 
